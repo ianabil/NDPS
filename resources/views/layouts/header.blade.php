0deb8e65382f76b46490344e59f691207d719d22
RupsaBose <rupsa2494@gmail.com>
2019-02-02 15:51:17 +0530

    Table created

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>LIS, Calcutta High Court</title>
+    <title>NDPS</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"> 
     @include('layouts.css_links')
