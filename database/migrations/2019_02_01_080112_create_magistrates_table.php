b87d2c19d663f69e31218fb95d418698a66623b9
ianabil <banabil90@gmail.com>
2019-02-02 12:13:42 +0530

    Changes in magistrate migration

--- database/migrations/2019_02_01_080112_create_magistrates_table.php
+++ database/migrations/2019_02_01_080112_create_magistrates_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMigratesTable extends Migration
+class CreateMagistratesTable extends Migration
 {
     /**
      * Run the migrations.
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
