0ccee1d65cd217cc4d1a8b23754538b65c767cb0
Anabil Bhattacharya <banabil90@gmail.com>
2019-02-12 19:38:24 +0530

    working on district and court synch

---
+++ app/Http/Controllers/MonthlyReportController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+use App\Narcotic;
+use App\District;
+use App\Unit;
+use App\Agency_detail;
+use App\Court_detail;
+use App\Magistrate;
+use App\Seizure;
+use App\Storage_detail;
+use App\User;
+use App\User_detail;
+use Carbon\Carbon;
+use DB;
+
+
+class MonthlyReportController extends Controller
+{
+    public function submitted_stakeholders(Request $request){
+        $month_of_report = date('Y-m-d', strtotime('01-'.$request->input('month_of_report')));
+        
+        $data = array();
+
+        $data['agency'] = Seizure::join('agency_details','seizures.agency_id','=','agency_details.agency_id')
+                                ->where([['month_of_report',$month_of_report],['submit_flag','S']])
+                                ->select('agency_details.agency_id','agency_details.agency_name')
+                                ->distinct()
+                                ->get();
+
+        $data['court'] = Seizure::join('court_details','seizures.court_id','=','court_details.court_id')
+                                ->where([['month_of_report',$month_of_report],['submit_flag','S']])
+                                ->select('court_details.court_id','court_details.court_name')
+                                ->distinct()
+                                ->get();
+
+        echo json_encode($data);
+    }
+
+
+    public function show_monthly_report(Request $request){
+        $month_of_report = date('Y-m-d', strtotime('01-'.$request->input('month_of_report')));
+        $stakeholder = $request->input('stakeholder');
+
+        
+        // For dataTable :: STARTS
+        $columns = array( 
+            0 =>'Narcotic Nature',
+            1 =>'Seize Quantity',
+            2 =>'Seizure Date',
+            3 =>'Disposal Date',
+            4 =>'Disposal Quantity',
+            5 =>'Not Disposed Quantity',
+            6 =>'Storage Place',
+            7 =>'Case Details',
+            8 =>'Where',
+            9 =>'Certification Date',
+            10 => 'Remarks'
+        );
+
+        $sql="select s.*,u1.unit_name seizure_unit, s.disposal_quantity, u2.unit_name disposal_unit,
+        s.undisposed_quantity,u3.unit_name undisposed_unit_name, court_details.*, districts.*
+        from seizures s inner join units u1 on cast(s.unit_name as int)=u1.unit_id 
+        inner join units u2 on cast(s.unit_of_disposal_quantity as int)=u2.unit_id 
+        inner join units u3 on cast(s.undisposed_unit as int)=u3.unit_id
+        inner join court_details on s.certification_court_id = court_details.court_id
+        inner join districts on s.district_id = districts.district_id
+        where s.submit_flag='S' and s.user_name= '".$stakeholder."' and 
+        s.month_of_report = '".$month_of_report."' order by seizure_id";
+
+        $data['seizures']=DB::select($sql);
+
+        $record = array();
+
+        foreach($data['seizures'] as $seizures){
+            $report['Narcotic Nature'] = $seizures->drug_name;
+            $report['Seize Quantity'] = $seizures->quantity_of_drug. " ".$seizures->seizure_unit;
+            $report['Seizure Date'] = Carbon::parse($seizures->date_of_seizure)->format('d-m-Y');
+            $report['Disposal Date'] = Carbon::parse($seizures->date_of_disposal)->format('d-m-Y');
+            $report['Disposal Quantity'] = $seizures->disposal_quantity. " ".$seizures->disposal_unit;
+            $report['Not Disposed Quantity'] = $seizures->undisposed_quantity. " ".$seizures->undisposed_unit_name;
+            $report['Storage Place'] = $seizures->storage_location;
+            $report['Case Details'] = $seizures->case_details;
+            $report['Where'] = $seizures->court_name;
+            $report['Certification Date'] = Carbon::parse($seizures->date_of_certification)->format('d-m-Y');
+            $report['Remarks'] = $seizures->remarks;
+
+            $record[] = $report;
+
+        }
+
+        $json_data = array(
+            "draw" => intval($request->input('draw')),
+            "recordsTotal" => intval(sizeof($record)),
+            "recordsFiltered" =>intval(sizeof($record)),
+            "data" => $record
+        );
+        
+        echo json_encode($json_data);
+
+    }
+}
---
+++ resources/views/post_submission_preview.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.app') @section('content')
+<!-- Main content -->
+<div class="box box-default">
+    <div class="box-header with-border">
+        <h3 class="box-title" text-align="center"><strong>Post Submission Preview</strong></h3>
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
+        </div>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+        <div class="row">
+            <form class="form-inline">
+                <div class="col-sm-7">
+                    <div class="form-group">
+                        <label>
+                            <h3>Court/Agency: {{$data['seizures'][0]->user_name}}</h3>
+                        </label>
+                    </div>
+                </div>
+                <div class="cold-sm-5">
+                    <div class="form-group">
+                        <label>
+                            <h3>Report For The Month Of: {{$data['seizures'][0]->month_of_report}}</h3>
+                        </label>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+            <hr>
+        
+        <div id="srollable" style="overflow:auto;">
+                <table class="table table-bordered display" style="white-space: nowrap;">
+                    <thead>
+                        <tr>
+                            <th rowspan="1"><strong>Nature of Narcotic<br> Drugs / Controlled<br> Substance</strong></th>
+                            <th rowspan="1"><strong>Quantity of<br> Seized<br> Contraband</strong></th>
+                            <th rowspan="1"><strong>Date of Seizure</strong></th>
+                            <th rowspan="1"><strong>Disposal Date</strong></th>
+                            <th rowspan="1"><strong>Disposal Quantity</strong></th>
+                            <th rowspan="1"><strong>If not disposed,<br> quantity</strong></th>
+                            <th rowspan="1"><strong>Place of Storage<br> of seized drugs</strong></th>
+                            <th rowspan="1"><strong>Case Details</strong></th>
+                            <th rowspan="1"><strong>Applied for <br> Certification At</strong></th>
+                            <th><strong>Date of<br> Certification</strong></th>
+                            <th rowspan="1"><strong>Remarks</strong></th>
+                        </tr>
+                    </thead>
+                    <tbody id="tbody">
+                        @foreach($data['seizures'] as $seizures)
+                        <tr>
+                            <!--nature of drug-->
+                            <td>{{$seizures->drug_name}}</td>
+                            <!--quantity of narcotic drugs-->
+                            <td>{{$seizures->quantity_of_drug}}  {{$seizures->seizure_unit}}</td>
+                            <!--date of seizure-->
+                            <td>{{$seizures->date_of_seizure}}</td>
+                            <!--disposal date-->
+                            <td>{{$seizures->date_of_disposal}}</td>
+                            <!--disposal quantity-->
+                            <td>{{$seizures->disposal_quantity}}  {{$seizures->disposal_unit}}</td>
+                            <!--quantity of undisposed drugs-->
+                            <td>{{$seizures->undisposed_quantity}}  {{$seizures->undisposed_unit_name}}</td>
+                            <!--storage location-->
+                            <td>{{$seizures->storage_location}}</td>
+                            <!--case details-->
+                            <td>{{$seizures->case_details}}</td>
+                            <!--where applied for certification-->
+                            <td>{{$seizures->court_name}}</td>
+                            <!--date of certification-->
+                            <td>{{$seizures->date_of_certification}}</td>
+                            <!--Remarks-->
+                            <td>{{$seizures->remarks}}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+
+<script src="{{asset('js/jquery/jquery.min.js')}}"></script>
+
+<script>
+
+	$(document).ready(function(){
+        $(".table").dataTable({ 
+            "searching": false,
+            "paging" : false,
+            dom: 'Bfrtip',
+                    buttons: [ 
+                        {
+                            extend: 'pdfHtml5',
+                            orientation: 'landscape',
+                            pageSize: 'A3',
+                            exportOptions: {
+                                columns: ':visible',
+                                stripNewlines: false
+                            },
+                            title: '',
+                            messageTop: '',
+                            messageBottom: '',
+                            customize: function(doc) {
+                            }
+                        }
+                    ]
+        });
+    })
+</script>
