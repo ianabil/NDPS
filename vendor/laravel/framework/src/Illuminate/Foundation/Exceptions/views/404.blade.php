1692771f7289e5bf0e21defcd7946eea264cd962
ianabil <banabil90@gmail.com>
2019-03-06 14:28:00 +0530

    Customize Page Not Found Added

--- vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php
@@ -1,11 +1,22 @@
-@extends('errors::illustrated-layout')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Page Not Found</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @include('layouts.css_links')
+  
+</head>
 
-@section('code', '404')
-@section('title', __('Page Not Found'))
+<body style="font-family:'Times New Roman', Times, serif; background-color:#131e3a;">
+<div class="container-fluid">
+  <div class="row">
+    
+  <img src="{{asset('images/404-error.jpg')}}" alt="404 page not found" class="img-responsive">
+    
+  </div>
+</div>
 
-@section('image')
-    <div style="background-image: url({{ asset('/svg/404.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
-    </div>
-@endsection
-
-@section('message', __('Sorry, the page you are looking for could not be found.'))
+</body>
+@include('layouts.js_links')
+</html>
