de3417fe8e35ec2bc096226d66914101e8b93687
ianabil <banabil90@gmail.com>
2019-06-10 13:02:15 +0530

    Forgot password feature added + giving details of email id made mandatory while user creation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,13 @@
                                             <button type="submit" class="btn btn-primary">
                                                 {{ __('Login') }}
                                             </button>
-                                        </div>
+
+                                            @if (Route::has('password.request'))
+                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                {{ __('Forgot Your Password?') }}
+                                            </a>
+                                            @endif
+                                        </div>                                        
                                     </div>
                                 </form>
                             </div>
