4158903b244f5cf06505a0e3baccba928ec28884
ianabil <banabil90@gmail.com>
2019-08-13 16:14:15 +0530

    meta data modified login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,15 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>DDMS</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"> 
+    <meta name="Drug Disposal Monitoring System" content="DDMS is developed by the
+    Software Developers of Calcutta High Court and its purpose is to monitor all
+    the seizures and its disposals.">
+    <meta name="Software Developers" content="Anabil Bhattacharya, Rupsa Bose">
+    <meta name="Guide" content="Shri Kallol Chattopadhyay, Shri Abhranil Neogi">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title>DDMS</title>  
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
